3fd1ccf391818a2b17320aa3800d49f6fafd6506
Evren Ceyhan <webceyhan@ceymac.lan>
2020-05-31 17:42:10 +0200

    additional character fields added

--- src/app/shared/models/character.ts
+++ src/app/shared/models/character.ts
@@ -3,6 +3,11 @@ import { Model } from './model';
 export interface Character extends Model {
     name: string;
     alias: string;
+    realName?: string;
+    location?: string;
+    occupation?: string;
+    alignment: 'hero' | 'villain' | 'other';
+    powers: string[];
     imageURL: string;
     story: string;
 }
