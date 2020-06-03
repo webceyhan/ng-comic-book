dc6ab10b35761e62677922e549fe86d1a0671d1f
Evren Ceyhan <webceyhan@ceymac.lan>
2020-06-03 16:31:04 +0200

    character edit form converted to reactive

--- src/app/characters/character-edit-page/character-edit-page.component.ts
+++ src/app/characters/character-edit-page/character-edit-page.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
-import { CharacterService } from 'shared/services/character.service';
 import { Character } from 'shared/models/character';
+import { CharacterService } from 'shared/services/character.service';
 
 @Component({
     selector: 'app-character-edit-page',
@@ -10,27 +11,41 @@ import { Character } from 'shared/models/character';
     styleUrls: ['./character-edit-page.component.css'],
 })
 export class CharacterEditPageComponent implements OnInit {
-    character = {} as Character;
+    form: FormGroup;
 
     readonly genders = ['male', 'female'];
     readonly alignments = ['hero', 'villain', 'other'];
 
     constructor(
+        private fb: FormBuilder,
         private route: ActivatedRoute,
         private router: Router,
         private characterSvc: CharacterService
     ) {}
 
     get edit() {
-        return !!this.character.id;
+        return !!this.form.value.id;
     }
 
     ngOnInit(): void {
-        this.character = this.route.snapshot.data.character || {};
+        const character: Character = this.route.snapshot.data.character || {};
+
+        this.form = this.fb.group({
+            id: [character.id],
+            alignment: [character.alignment || '', Validators.required],
+            name: [character.name || '', Validators.required],
+            alias: [character.alias || ''],
+            imageURL: [character.imageURL || ''],
+            gender: [character.gender || '', Validators.required],
+            realName: [character.realName || ''],
+            location: [character.location || ''],
+            occupation: [character.occupation || ''],
+            story: [character.story || '', Validators.required],
+        });
     }
 
     async onSave() {
-        await this.characterSvc.save(this.character);
+        await this.characterSvc.save(this.form.value);
         this.onCancel();
     }
 
